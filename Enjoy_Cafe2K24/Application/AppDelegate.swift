08a4505b85362ab33216312f1335cef5c06a6db9
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-10-09 00:39:10 +0630

    initial commit

---
+++ Enjoy_Cafe2K24/Application/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Enjoy_Cafe2K24
+//
+//  Created by Aung Kyaw Phyo on 09/10/2024.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Enjoy_Cafe2K24/Views/LoginScreenModule/LoginScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginScreenViewController.swift
+//  Enjoy_Cafe2K24
+//
+//  Created by Aung Kyaw Phyo on 09/10/2024.
+//
+
+import UIKit
+
+class LoginScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
