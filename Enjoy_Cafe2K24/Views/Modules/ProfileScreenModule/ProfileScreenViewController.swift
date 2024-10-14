e35e3a2183329a6252625bc46aa3375edd239220
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-10-15 00:55:18 +0630

    profile commit

--- Enjoy_Cafe2K24/Views/Modules/ProfileScreenModule/ProfileScreenViewController.swift
+++ Enjoy_Cafe2K24/Views/Modules/ProfileScreenModule/ProfileScreenViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ProfileScreenViewController: EnjoyViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var userImageView: EnjoyImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var optionsTable: UITableView!
+    
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +23,6 @@ class ProfileScreenViewController: EnjoyViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
---
+++ Enjoy_Cafe2K24/Views/UIComponents/TableViewCells/ProfileTableViewCell/ProfileTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileTableViewCell.swift
+//  Enjoy_Cafe2K24
+//
+//  Created by Aung Kyaw Phyo on 15/10/2024.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
